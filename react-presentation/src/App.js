a20aae3bfe7beb4db2e627027571e3bf929e020b
mmaria159 <macovei.maria1999@gmail.com>
2022-05-21 09:41:07 +0300

    feat: static counter

--- react-presentation/src/App.js
+++ react-presentation/src/App.js
@@ -2,7 +2,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App"></div>
+        <div class="section-1">
+           <div class="display">
+             <div class="counter-value" id="counter-value">
+                0
+             </div>
+           </div>
+           <div class="actions">
+               <button class="plus" id="plus">Plus</button>
+               <button class="minus" id="minus">Minus</button>
+           </div>
+        </div>
   );
 }
 
